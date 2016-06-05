f1b74bbef1016a8993cde3df45325cfdc0156fae
Teddy Widom <theodore.widom@gmail.com>
2016-06-05 15:21:43 -0400

    Allow specific include/exclude clauses
    This allows us to have one main directory such as:
    
    /home/teddy/backed_up
    
    But also to specify some other important things that for whatever reason
    cannot be stored there, such as:
    
    /home/teddy/.bashrc
    /home/teddy/.zshrc

--- backup.sh
+++ backup.sh
@@ -14,8 +14,6 @@ set -eou pipefail
 #
 #
 # Set up an S3 bucket to use for backup.
-# For lower AWS costs, set up a Lifecyle rule on the bucket to transition
-# objects to "Standard - Infrequent Access" S3 Storage Class.
 # Add an AWS user with permissions just for that bucket. Example IAM policy:
 # {
 #     "Version": "2012-10-17",
@@ -42,7 +40,8 @@ set -eou pipefail
 #   AWS_ACCESS_KEY_ID=<your_aws_access_key>
 #   AWS_SECRET_ACCESS_KEY=<your_aws_secret_access_key>
 #   S3_BUCKET_NAME=<name_of_your_s3_bucket>
-#   LOCAL_DIRECTORY_TO_BACK_UP=<your_local_directory_to_recursively_back_up>
+#   LOCAL_DIRECTORY_TO_BACK_UP=/home/teddy
+#   INCLUDE_EXCLUDE_CLAUSE="--include /home/teddy/backed_up --include /home/teddy/.bashrc --exclude /home/teddy"
 #
 # Ensure you record the values you put in ~/.duplicity/config encrypted and offsite somewhere.
 
@@ -53,25 +52,40 @@ export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export S3_BUCKET_NAME
 export LOCAL_DIRECTORY_TO_BACK_UP
+export INCLUDE_EXCLUDE_CLAUSE
 
+
+# To backup, run:
 duplicity \
     --verbosity notice \
     --s3-use-new-style \
+    --s3-use-ia \
+    --s3-use-multiprocessing \
     --asynchronous-upload \
     --volsize 25 \
     --log-file ~/.duplicity/duplicity.log \
-    $LOCAL_DIRECTORY_TO_BACK_UP s3+http://$S3_BUCKET_NAME/
+    $INCLUDE_EXCLUDE_CLAUSE \
+    /home/teddy s3+http://$S3_BUCKET_NAME/
 
-# To restore, run something like:
+
+# To list files in backup, run:
+# duplicity list-current-files \
+#     --s3-use-new-style \
+#     s3+http://$S3_BUCKET_NAME
+
+
+# To restore, run `$ mkdir ~/duplicity-restore` then:
 # duplicity restore \
 #     --verbosity notice \
 #     --s3-use-new-style \
 #     [--time '2016-01-03'] \
 #     [--file-to-restore foo/bar/baz.txt] \
-#     s3+http://$S3_BUCKET_NAME $LOCAL_DIRECTORY_TO_RESTORE_TO
+#     s3+http://$S3_BUCKET_NAME ~/duplicity-restore/foo/bar/baz-restored.txt
+
 
 unset PASSPHRASE
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
 unset S3_BUCKET_NAME
 unset LOCAL_DIRECTORY_TO_BACK_UP
+unset INCLUDE_EXCLUDE_CLAUSE
