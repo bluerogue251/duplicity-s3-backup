f3b43a52f1536b7196caee88bfbc795d8cc601e3
Teddy Widom <theodore.widom@gmail.com>
2016-06-07 00:12:32 -0400

    Add verify script

---
+++ verify.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eou pipefail
+
+source "$HOME/.duplicity/config"
+
+export PASSPHRASE
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export S3_BUCKET_NAME
+export LOCAL_DIRECTORY_TO_BACK_UP
+
+RELATIVE_PATH_TO_VERIFY=$1
+
+duplicity verify \
+    --name trw-personal \
+    --verbosity Notice \
+    --compare-data \
+    --file-to-restore $RELATIVE_PATH_TO_VERIFY \
+    s3://s3.amazonaws.com/$S3_BUCKET_NAME $LOCAL_DIRECTORY_TO_BACK_UP/$RELATIVE_PATH_TO_VERIFY
+
+unset PASSPHRASE
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset S3_BUCKET_NAME
+unset LOCAL_DIRECTORY_TO_BACK_UP
